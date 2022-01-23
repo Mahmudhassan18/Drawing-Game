26516b896b0008f9101732437bb41b578876ccbb
Mahmud Hassan <mahmodhasan1001@gmail.com>
2022-01-23 21:16:11 +0200

    v1.1 Reorganizing the code

--- Cell.py
+++ Cell.py
@@ -1,132 +1,71 @@
 import pygame
 import random
 
+visitedCells = set()
+
 
 class Cell:
     def __init__(self, width, position, color):
         self.width = width
         self.position = position
-        self.sides = [True, True, True, True]
         self.visited = 0
         self.color = color
 
     def draw(self, screen):
+        for cell in visitedCells:
+            pygame.draw.rect(screen, cell.color, (
+                cell.position[0], cell.position[1], cell.width, cell.width))
         pygame.draw.rect(screen, self.color, (self.position[0], self.position[1], self.width, self.width))
-        # if self.visited == 1:
-        #     pygame.draw.rect(screen, self.color, (self.position[0], self.position[1], self.width, self.width))
-        # if self.sides[0]:
-        #     pygame.draw.line(screen, (0, 0, 0), self.position, (self.position[0] + self.width, self.position[1]))
-        # if self.sides[1]:
-        #     pygame.draw.line(screen, (0, 0, 0), (self.position[0] + self.width, self.position[1]),
-        #                      (self.position[0] + self.width, self.position[1] + self.width))
-        # if self.sides[2]:
-        #     pygame.draw.line(screen, (0, 0, 0), (self.position[0], self.position[1] + self.width),
-        #                      (self.position[0] + self.width, self.position[1] + self.width))
-        # if self.sides[3]:
-        #     pygame.draw.line(screen, (0, 0, 0), self.position, (self.position[0], self.position[1] + self.width))
 
 
 class MovingCell(Cell):
-    def __init__(self, width, position, color):
-        Cell(width, position, color)
-        self.x = position[0]
-        self.y = position[1]
-        self.color = color
-        self.width = width
+    def __init__(self, width, position, color, screenSize):
+        super(MovingCell, self).__init__(width, position, color)
         self.deleteMode = 0
-        self.i = 0
-        self.j = 0
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.width))
+        self.screenSize = screenSize
 
     def control(self):
         pressed_keys = pygame.key.get_pressed()
-
+        add = True
         if pressed_keys[pygame.K_SPACE]:
             if self.deleteMode == 1:
                 self.deleteMode = 0
             else:
                 self.deleteMode = 1
+        for cell in visitedCells:
+            if cell.position == self.position:
+                add = False
+        if add:
+            visitedCells.add(Cell(self.width, self.position, (128, 0, 128)))
 
-        if pressed_keys[pygame.K_LEFT] and self.x > 0:
-            self.x -= self.width
-        elif pressed_keys[pygame.K_RIGHT] and self.x < 600 - self.width:
-            self.x += self.width
-        elif pressed_keys[pygame.K_UP] and self.y > 0:
-            self.y -= self.width
-        elif pressed_keys[pygame.K_DOWN] and self.y < 600 - self.width:
-            self.y += self.width
+        if pressed_keys[pygame.K_LEFT] and self.position[0] > 0:
+            self.position = (self.position[0] - self.width, self.position[1])
+        elif pressed_keys[pygame.K_RIGHT] and self.position[0] < self.screenSize - self.width:
+            self.position = (self.position[0] + self.width, self.position[1])
+        elif pressed_keys[pygame.K_UP] and self.position[1] > 0:
+            self.position = self.position = (self.position[0], self.position[1] - self.width)
+        elif pressed_keys[pygame.K_DOWN] and self.position[1] < self.screenSize - self.width:
+            self.position = self.position = (self.position[0], self.position[1] + self.width)
 
-    def checkVisited(self, cells):
-        for i in range(len(cells)):
-            for j in range(len(cells[0])):
-                if cells[i][j].position == (self.x, self.y):
-                    if self.deleteMode == 1:
-                        cells[i][j].visited = 0
-                    else:
-                        cells[i][j].visited = 1
-
-    def getUnvisitedNeighbors(self, cells):
-        rightNeig = cells[(self.y // self.width)][((self.x // self.width) + 1) % len(cells[0])]
-        leftNeig = cells[(self.y // self.width)][((self.x // self.width) - 1) % len(cells[0])]
-        upperNeig = cells[((self.y // self.width) - 1) % len(cells[0])][(self.x // self.width)]
-        bottomNeig = cells[((self.y // self.width) + 1) % len(cells[0])][(self.x // self.width)]
-        unvisitedNeighbors = []
-        if self.x == 0 and self.y == 0:
-            # if rightNeig.visited == 0:
-            unvisitedNeighbors.append(rightNeig)
-            # if bottomNeig.visited == 0:
-            unvisitedNeighbors.append(bottomNeig)
-
-        if self.x == 0 and self.y != 0:
-            # if rightNeig.visited == 0:
-            unvisitedNeighbors.append(rightNeig)
-            # if upperNeig.visited == 0:
-            unvisitedNeighbors.append(upperNeig)
-            # if bottomNeig.visited == 0:
-            unvisitedNeighbors.append(bottomNeig)
-
-        if self.x != 0 and self.y == 0:
-            # if rightNeig.visited == 0:
-            unvisitedNeighbors.append(rightNeig)
-            # if leftNeig.visited == 0:
-            unvisitedNeighbors.append(leftNeig)
-            # if bottomNeig.visited == 0:
-            unvisitedNeighbors.append(bottomNeig)
-
-        if self.x != 0 and self.y != 0:
-            # if rightNeig.visited == 0:
-            unvisitedNeighbors.append(rightNeig)
-            # if upperNeig.visited == 0:
-            unvisitedNeighbors.append(upperNeig)
-            # if bottomNeig.visited == 0:
-            unvisitedNeighbors.append(bottomNeig)
-            # if leftNeig.visited == 0:
-            unvisitedNeighbors.append(leftNeig)
-
-        return unvisitedNeighbors
+    def getNeighbors(self, cells):
+        neighbors = []
+        rightNeighbor = cells[(self.position[1] // self.width)][((self.position[0] // self.width) + 1)]
+        leftNeighbor = cells[(self.position[1] // self.width)][((self.position[0] // self.width) - 1)]
+        upperNeighbor = cells[((self.position[1] // self.width) - 1)][(self.position[0] // self.width)]
+        bottomNeighbor = cells[((self.position[1] // self.width) + 1)][(self.position[0] // self.width)]
+        if self.position[0] + (2 * self.width) <= self.screenSize:
+            neighbors.append(rightNeighbor)
+        if self.position[0] - self.width >= 0:
+            neighbors.append(leftNeighbor)
+        if self.position[1] + (2 * self.width) <= self.screenSize:
+            neighbors.append(bottomNeighbor)
+        if self.position[1] - self.width >= 0:
+            neighbors.append(upperNeighbor)
+        return neighbors
 
     def randomDrawing(self, cells):
-        unvisitedNeighbors = self.getUnvisitedNeighbors(cells)
-        if len(unvisitedNeighbors) != 0:
-            randNeighbor = random.choice(unvisitedNeighbors)
-            self.x = randNeighbor.position[0]
-            self.y = randNeighbor.position[1]
-            randNeighbor.visited = 1
-
-    def drawImage(self, color, cells):
-        if self.j < (len(cells[0])):
-            self.x = cells[self.j][self.i].position[0]
-            self.y = cells[self.j][self.i].position[1]
-            cells[self.j][self.i].color = color[self.j][self.i]
-            if self.i < (len(cells[0]) - 1):
-                self.i += 1
-            else:
-                self.i = 0
-                self.j += 1
-
-
-
-
-
+        neighbors = self.getNeighbors(cells)
+        if len(neighbors) != 0:
+            randNeighbor = random.choice(neighbors)
+            self.position = randNeighbor.position
+            visitedCells.append(randNeighbor)
--- main.py
+++ main.py
@@ -13,27 +13,18 @@ FramePerSec = pygame.time.Clock()
 SCREENWIDTH = 600
 SCREENHEIGHT = 600
 
-
 screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption("Game")
 
-mainMenu = MainMenu("Main Menu", screen)
-mainMenu.run()
-cellWidth = mainMenu.cellWidth
-FPS = mainMenu.fps
+cellWidth = 10
+FPS = 60
 
 image = Image.open("middle.jpg")
 image = image.resize((SCREENHEIGHT // cellWidth, SCREENHEIGHT // cellWidth), Image.ANTIALIAS)
 image_array = np.array(image)
 
-cells = []
-for i in range(SCREENHEIGHT // cellWidth):
-    row = []
-    for j in range(SCREENWIDTH // cellWidth):
-        row.append(Cell(cellWidth, (j * cellWidth, i * cellWidth), (255, 255, 255)))
-    cells.append(row)
+movingCell = MovingCell(cellWidth, (0, 0), (0, 0, 0), SCREENHEIGHT)
 
-movingCell = MovingCell(cellWidth, (0, 0), (0, 0, 0))
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -41,18 +32,8 @@ while True:
 
     screen.fill((255, 255, 255))
 
-    for i in range(SCREENHEIGHT // cellWidth):
-        for j in range(SCREENWIDTH // cellWidth):
-            cells[i][j].draw(screen)
-
     movingCell.draw(screen)
-    movingCell.checkVisited(cells)
-
-    if mainMenu.runningOption == "randomDrawing":
-        movingCell.drawImage(image_array, cells)
-        # movingCell.randomDrawing(cells)
-    else:
-        movingCell.control()
+    movingCell.control()
 
     pygame.display.update()
     FramePerSec.tick(FPS)
